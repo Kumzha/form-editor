836ba33c486cc03684a87e29e10129dc83c4a133
Kumzha <kumza.dominykas@gmail.com>
2025-02-26 10:54:05 +0200

    updated navbar

--- src/components/myComponents/navbar.tsx
+++ src/components/myComponents/navbar.tsx
@@ -13,8 +13,8 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ form }) => {
   return (
-    <div className="navbar h-10 bg-[#6a6a6d] fixed top-0 left-0 right-0 z-50 border-b-2 border-[#000000]">
-      <div className="navbar-start w-[15%]">
+    <div className="h-16 bg-[#6a6a6d] fixed top-0 left-0 right-0 z-50 border-b-2 border-[#000000] flex flex-row">
+      <div className="w-[15%]">
         <div className="h-16 p-3 flex items-center justify-center">
           <div className="w-5 h-5 bg-white rounded-full"></div>
           <span className="inline-flex items-center text-xl font-bold font-sans pl-3 m-0 leading-none text-white select-none">
@@ -22,7 +22,7 @@ const Navbar: React.FC<NavbarProps> = ({ form }) => {
           </span>
         </div>
       </div>
-      <div className="navbar-center w-[70%] hidden lg:flex items-center text-white">
+      <div className="w-[70%] hidden lg:flex items-center text-white">
         {form ? (
           <div className="flex flex-col gap-2 w-full items-center font-semibold">
             <span className="whitespace-nowrap">{form.name}</span>
@@ -30,7 +30,7 @@ const Navbar: React.FC<NavbarProps> = ({ form }) => {
         ) : null}
         {/* {form ? <Progress form={form} className="w-[40%]" /> : null} */}
       </div>
-      <div className="navbar-end w-[15%]"></div>
+      <div className="w-[15%]"></div>
     </div>
   );
 };
